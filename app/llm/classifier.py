5dc3b4a4003db54e896c5e5625b9b3f4fb9aca23
maskmohammed <154214372+maskmohammed@users.noreply.github.com>
2026-04-19 16:05:31 +0000

    final change llm

--- app/llm/classifier.py
+++ app/llm/classifier.py
@@ -4,7 +4,7 @@ import requests
 
 
 class LLMClassifier:
-    def __init__(self, model_name="qwen2.5:0.5b-instruct", base_url="http://localhost:11434/api/generate"):
+    def __init__(self, model_name="tinyllama", base_url="http://localhost:11434/api/generate"):
         self.model_name = model_name
         self.base_url = base_url
 
